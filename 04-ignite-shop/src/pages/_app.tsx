6632713fa891079a7ff7723a0d720228b68b3a1e
Thainá Pires <thainaspiress@gmail.com>
2023-07-02 21:34:28 -0300

    ignite-shop: adding base code with styles and configs

---
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps } from 'next/app'
+import logoImg from '../assets/logo.svg'
+import { globalStyles } from '../styles/global'
+import { Container, Header } from '../styles/pages/app'
+
+import Image from 'next/image'
+
+globalStyles()
+
+console.log(logoImg.src)
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt=""/>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
+}
