70bb415101130504b0b17c9e5bec396768b2f9ce
Thainá Pires <thainaspiress@gmail.com>
2023-07-05 00:17:15 -0300

    ignite-shop: basic info for SEO

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -2,6 +2,7 @@ import { stripe } from "@/src/lib/stripe"
 import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product"
 import axios from "axios"
 import { GetStaticPaths, GetStaticProps } from "next"
+import Head from "next/head"
 import Image from "next/image"
 import { useRouter } from "next/router"
 import { useState } from "react"
@@ -47,19 +48,24 @@ export default function Product({product}: ProductProps) {
   }
 
   return (
-    <ProductContainer>
-      <ImageContainer>
-        <Image src={product.imageUrl} width={520} height={480} alt=""/>
-      </ImageContainer>
-      <ProductDetails>
-        <h1>{product.name}</h1>
-        <span>{product.price}</span>
-        <p>{product.description}</p>
-        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
-          Comprar agora
-        </button>
-      </ProductDetails>
-    </ProductContainer>
+    <>
+      <Head>
+        <title>{product.name} - Ignite Shop</title>
+      </Head>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt=""/>
+        </ImageContainer>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
+          <p>{product.description}</p>
+          <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
   )
 }
 
--- 04-ignite-shop/src/pages/success.tsx
+++ 04-ignite-shop/src/pages/success.tsx
@@ -4,6 +4,7 @@ import { GetServerSideProps } from "next";
 import { stripe } from "../lib/stripe";
 import Stripe from "stripe";
 import Image from "next/image";
+import Head from "next/head";
 
 interface SuccessProps {
     customerName: string;
@@ -15,20 +16,26 @@ interface SuccessProps {
 
 export default function Success({ customerName, product }: SuccessProps){
     return (
-        <SuccessContainer>
-            <h1>Compra efetuada!</h1>
-            <ImageContainer>
-                <Image src={product.imageUrl} width={120} height={110} alt=""/>
-            </ImageContainer>
+        <>
+            <Head>
+                <title>Compra efetuada - Ignite Shop</title>
+                <meta name="robots" content="noindex"/>
+            </Head>
+            <SuccessContainer>
+                <h1>Compra efetuada!</h1>
+                <ImageContainer>
+                    <Image src={product.imageUrl} width={120} height={110} alt=""/>
+                </ImageContainer>
 
-            <p>
-                Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
-            </p>
+                <p>
+                    Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
+                </p>
 
-            <Link href="/">
-                Voltar ao catalogo
-            </Link>
-        </SuccessContainer>
+                <Link href="/">
+                    Voltar ao catalogo
+                </Link>
+            </SuccessContainer>
+        </>
     )
 }
 
